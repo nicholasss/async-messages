db1ed14a9b5abd7cf8c1ad511b9e19dfc5b4ec45
Nicholas <nicholasss@users.noreply.github.com>
2025-07-21 16:48:09 -0400

    new client package

---
+++ internal/client/client.go
@@ -0,0 +1,74 @@
+// Package client implements a client to be created and perform common operations
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/nicholasss/async-messages/internal/msg"
+)
+
+type Client struct {
+	SecretKey []byte
+	Client    http.Client
+	Queue     *msg.Queue
+	Name      string
+	Server    string
+}
+
+func NewClient(name string) (*Client, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+	secretKey := []byte(os.Getenv("HMAC_SECRET"))
+
+	queue := msg.NewQueue()
+
+	return &Client{
+		SecretKey: secretKey,
+		Client:    *http.DefaultClient,
+		Queue:     queue,
+		Name:      name,
+		Server:    "http://localhost:8080/echo",
+	}, nil
+}
+
+func (c *Client) AddToQueue(msg *msg.Message) {
+	c.Queue.Enqueue(*msg)
+}
+
+func (c *Client) SendFromQueue() error {
+	msgToSend, ok := c.Queue.Dequeue()
+	if !ok {
+		return errors.New("unable to send message due to issue dequeuing message")
+	}
+
+	// verify message for sending
+	err := msg.VerifyMessage(&msgToSend, c.SecretKey)
+	if err != nil {
+		return err
+	}
+
+	msgData, err := json.Marshal(msgToSend)
+	if err != nil {
+		return err
+	}
+	msgDataReader := bytes.NewBuffer(msgData)
+
+	res, err := c.Client.Post(c.Server, "application/json", msgDataReader)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("attempted to send message. response status code of '%s %d'", res.Status, res.StatusCode)
+	}
+
+	return nil
+}

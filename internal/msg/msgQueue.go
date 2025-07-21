fc467fe1854352049e34fc9725d080efc22a14bc
Nicholas <nicholasss@users.noreply.github.com>
2025-07-21 15:50:58 -0400

    removed printing

--- internal/msg/msgQueue.go
+++ internal/msg/msgQueue.go
@@ -1,7 +1,5 @@
 package msg
 
-import "log"
-
 type queue struct {
 	msgs []Message
 }
@@ -21,14 +19,14 @@ func (q *queue) IsEmpty() bool {
 
 func (q *queue) Enqueue(newMsg Message) {
 	q.msgs = append(q.msgs, newMsg)
-	log.Printf("[QUEUE] Enqueued %s", newMsg)
+	// log.Printf("[QUEUE] Enqueued %s", newMsg)
 }
 
 // in order to increase efficiency look into:
 // - ring buffers or linked lists
 func (q *queue) Dequeue() (Message, bool) {
 	if q.IsEmpty() {
-		log.Printf("[QUEUE] Unable to dequeue in an empty queue.")
+		// log.Printf("[QUEUE] Unable to dequeue in an empty queue.")
 		return Message{}, false
 	}
 	nextMsg := q.msgs[0]
@@ -36,6 +34,6 @@ func (q *queue) Dequeue() (Message, bool) {
 	// reslicing the queue, not efficient but functional
 	q.msgs = q.msgs[1:]
 
-	log.Printf("[QUEUE] Dequeued %s", nextMsg)
+	// log.Printf("[QUEUE] Dequeued %s", nextMsg)
 	return nextMsg, true
 }

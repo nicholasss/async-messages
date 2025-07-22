f95154f3e9681f6eefe4e13ce5a2cce00d24451b
Nicholas <nicholasss@users.noreply.github.com>
2025-07-22 18:33:24 -0400

    added another test

--- internal/msg/rawMsg_test.go
+++ internal/msg/rawMsg_test.go
@@ -187,3 +187,55 @@ func TestMessageDataForSigning(t *testing.T) {
 		}
 	}
 }
+
+func TestCreateSignature(t *testing.T) {
+	tt := []struct {
+		rawMsg        RawMessage
+		wantSignature string
+	}{
+		{
+			rawMsg: RawMessage{
+				ToName:     "bob",
+				ToVessel:   "snow",
+				FromName:   "kevin",
+				FromVessel: "liberty",
+				Subject:    "Tuesday",
+				Body:       "I am planning on proceeding on tuesday since there is a break in the weather",
+			},
+			wantSignature: "2d18d51dee10b1c5b5e673652b9744ebc8f0900c114cb34262b1429738a116f9",
+		},
+		{
+			rawMsg: RawMessage{
+				ToName:     "kevin",
+				ToVessel:   "liberty",
+				FromName:   "bob",
+				FromVessel: "snow",
+				Subject:    "Re: Tuesday",
+				Body:       "I will need to wait longer because of needed repair work. Hope to catch up.",
+			},
+			wantSignature: "7bf816982b5d8c52822a366e68d9fd2662c9192e4a86473d4dfdfbd29cfd64d5",
+		},
+		{
+			rawMsg: RawMessage{
+				ToName:     "bob",
+				ToVessel:   "snow",
+				FromName:   "kevin",
+				FromVessel: "liberty",
+				Subject:    "Re: Re: Tuesday",
+				Body:       "Good idea. Take your time. I will send out a message when we get into Cambridge Bay.",
+			},
+			wantSignature: "1a936de118b68b1cea9469ec5cd22b7bd65bef71027eb35b88c931ad66eaa870",
+		},
+	}
+
+	for _, tc := range tt {
+		gotSignature, err := tc.rawMsg.createSignature(rawMsgSecretKey)
+		if err != nil {
+			t.Errorf("create signature failed unexpectedly due to: %q", err)
+		}
+
+		if gotSignature != tc.wantSignature {
+			t.Errorf("signature mismatch. got=%s want=%s", gotSignature, tc.wantSignature)
+		}
+	}
+}

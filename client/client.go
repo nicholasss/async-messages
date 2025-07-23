374c486cfc1651c280c9289136eecd6efd5d7a44
Nicholas <nicholasss@users.noreply.github.com>
2025-07-23 16:01:38 -0400

    updated client for testing

--- client/client.go
+++ client/client.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/nicholasss/async-messages/internal/client"
 )
 
 func main() {
-	c, err := client.NewClient("Craig", "AlfredoExpress")
+	c, err := client.NewClientConfig("Craig", "AlfredoExpress")
 	if err != nil {
-		log.Printf("unable to create new client due to: %q", err)
+		fmt.Printf("unable to create new client config due to: %q\n", err)
 		return
 	}
 
-	// sending one of the messages
-	err = c.SendAllFromQueue()
+	err = c.StartClient()
 	if err != nil {
-		log.Printf("unable to send from queue due to: %q", err)
-	} else {
-		log.Printf("sent queued message")
+		fmt.Printf("unable to start new client due to: %q\n", err)
 	}
 
-	log.Printf("Queue dump >\n%s", c.Queue.QueueSummary())
+	err = c.WriteMessageIntoQueue("Bob", "Snow", "Shovel", "We should get going on tuesday.")
+	if err != nil {
+		fmt.Printf("cannot write message due to: %q\n", err)
+	}
+
+	err = c.SendAllFromQueue()
+	if err != nil {
+		fmt.Printf("cannot send due to: %q\n", err)
+	}
+
+	fmt.Printf("Queue Summary: %s\n", c.Outbox.QueueSummary())
 }
